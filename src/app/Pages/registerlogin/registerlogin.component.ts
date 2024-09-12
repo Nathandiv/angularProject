91edae91d33be71d2b6c7393ff67d2e5ab39eea7
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-12 13:51:47 +0200

    added pages (components)

---
+++ src/app/Pages/registerlogin/registerlogin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registerlogin',
+  standalone: true,
+  imports: [],
+  templateUrl: './registerlogin.component.html',
+  styleUrl: './registerlogin.component.css'
+})
+export class RegisterloginComponent {
+
+}
