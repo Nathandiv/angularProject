17712ce76f2d56da12d1775a431bbdd6005cd30e
Nathan Hlungwani <k.n.hlungwani@gmail.com>
2024-09-16 00:00:40 +0200

    added navbar and footer on login page

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
+import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [NavComponent,FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
