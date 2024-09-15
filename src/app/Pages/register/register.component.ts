e7fd758fefd23aa2e5cb512de4798736179d3336
Nathan Hlungwani <k.n.hlungwani@gmail.com>
2024-09-16 00:02:47 +0200

    added navbar and footer on form register page

--- src/app/Pages/register/register.component.ts
+++ src/app/Pages/register/register.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
+import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [NavComponent,FooterComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
