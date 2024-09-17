d0714e309172e1a274f28c7f578d04eafea11921
Nathan Hlungwani <k.n.hlungwani@gmail.com>
2024-09-17 20:16:39 +0200

    added login user navbar

---
+++ src/app/UI/Shared-UI/nav-login/nav-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-login.component.html',
+  styleUrl: './nav-login.component.css'
+})
+export class NavLoginComponent {
+
+}
