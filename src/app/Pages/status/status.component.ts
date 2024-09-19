a3e2d8009cfffdfd05e3af76319cd3cb343d255b
Nathan Hlungwani <k.n.hlungwani@gmail.com>
2024-09-19 21:17:48 +0200

    added status on navigation

--- src/app/Pages/status/status.component.ts
+++ src/app/Pages/status/status.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
 
 @Component({
   selector: 'app-status',
   standalone: true,
-  imports: [],
+  imports: [NavComponent],
   templateUrl: './status.component.html',
   styleUrl: './status.component.css'
 })
