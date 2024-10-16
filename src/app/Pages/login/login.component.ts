2c879672cf3b7852deccb28aa6773f00318efc2f
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-16 13:54:47 +0200

    added inject for services and

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
 import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../Services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,11 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  authService = inject(AuthService)
+  email: any;
+password: any;
+username: any;
 
-  constructor(private router: Router){}
 
-  login(){
-    this.router.navigate(['/']);
-
-  }
-
+ 
 }
