b58b24d58a1f4a391c6aacab7a126c3d1007e87d
Nathandiv <k.n.hlungwani@gmail.com>
2024-10-18 09:32:05 +0200

    added navbar and footer

--- src/app/Pages/registerlogin/registerlogin.component.ts
+++ src/app/Pages/registerlogin/registerlogin.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
+import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
 
 @Component({
   selector: 'app-registerlogin',
   standalone: true,
-  imports: [],
+  imports: [NavComponent,FooterComponent],
   templateUrl: './registerlogin.component.html',
   styleUrl: './registerlogin.component.css'
 })
