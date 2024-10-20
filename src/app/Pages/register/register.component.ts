8d982e632c68d460092147dcdc295ca7cbed2860
Nathan Hlungwani <k.n.hlungwani@gmail.com>
2024-10-20 13:30:50 +0200

    added changes on both loging and register

--- src/app/Pages/register/register.component.ts
+++ src/app/Pages/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
 import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [NavComponent,FooterComponent],
+  imports: [NavComponent,FooterComponent,RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
--- src/app/Pages/registerlogin/registerlogin.component.ts
+++ src/app/Pages/registerlogin/registerlogin.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../UI/Shared-UI/nav/nav.component';
 import { FooterComponent } from '../../UI/Shared-UI/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registerlogin',
   standalone: true,
-  imports: [NavComponent,FooterComponent],
+  imports: [NavComponent,FooterComponent,RouterLink],
   templateUrl: './registerlogin.component.html',
   styleUrl: './registerlogin.component.css'
 })
